6919b2fe3e71d605ff0347b34962cb01a00a798d
Vladimir Aralin <ulialiainer@gmail.com>
2020-01-05 00:54:41 +0300

    #1 Добавил прототип солюшки.

---
+++ src/MythicTracker.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MythicTracker.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
