87095a16a3d7793c81fe9bfa7ddddf1a2ba86c17
Suprerum <48028090+Suprerum@users.noreply.github.com>
2020-01-05 15:12:54 +0600

    1

---
+++ src/MythicTracker.ConsoleApp/IGameStateObserver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MythicTracker.ConsoleApp
+{
+    public interface IGameStateObserver
+    {
+        event GameStateChangedEventArgs Notify;
+        
+        public void Start()
+        {
+
+        }
+
+        public void Finish()
+        {
+
+        }
+    }
+}
