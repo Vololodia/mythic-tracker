6fbae7774e9d32c1776d271db6e4a85604f4744e
Artem Klerikh <48028090+Suprerum@users.noreply.github.com>
2020-02-14 18:50:03 +0600

    PlayerDeck (#14)
    Resolve #13

---
+++ src/MythicTracker.Application/GameDeck/DeckCard.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MythicTracker.Application.GameDeck
+{
+    public class DeckCard
+    {
+        private int[] Positions { get; set; }
+    }
+}
